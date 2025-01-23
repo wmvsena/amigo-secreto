b278119a6604c6a60871fbd17f0071967f4124cd
Marcos Sena <wmvsena@gmail.com>
2025-01-23 10:36:23 -0300

    projeto finalizado

--- app.js
+++ app.js
@@ -1,1 +1,44 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let listaDeNomesSecretos = [];
+
+function adicionarNome() {
+    const nomeDoAmigo = document.getElementById("amigo");
+    const nome = nomeDoAmigo.value.trim();
+
+    if (nome =="") {
+        alert("insira um nome")
+        return
+    }
+    
+    listaDeNomesSecretos.push(nome);
+    console.log(listaDeNomesSecretos);
+    nomeDoAmigo.value = "";
+    mostrarAmigos();
+}
+
+function mostrarAmigos() {
+        const lista = document.querySelector("#listaAmigos");
+        lista.innerHTML = "";
+        console.log("Lista limpa")
+
+        for (let i = 0; i < listaDeNomesSecretos.length; i++){
+            const li = document.createElement("li")
+            li.textContent = listaDeNomesSecretos[i]
+            lista.appendChild(li);
+        }
+
+}
+
+function sortearAmigo() {
+    if (listaDeNomesSecretos.length < 2 || listaDeNomesSecretos.length === 0) { 
+        alert("Não há amigos para sortear. Adicione alguns nomes.");
+        return;
+    } 
+ 
+    const indiceAleatorio = Math.floor(Math.random() * listaDeNomesSecretos.length);
+    const amigoSorteado = listaDeNomesSecretos[indiceAleatorio];
+    const resultado = document.querySelector("#resultado");
+    
+    resultado.innerHTML = `O amigo secreto sorteado é: <strong>${amigoSorteado}</strong>`;
+
+}
